f1be8d47c401e136c8f58bea870e181cb22551a3
Kayode <kayodefredrickbalogun@gmail.com>
2024-06-17 14:42:37 +0100

    Initial Commit

---
+++ SwiftUIWeather/SwiftUIWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWeatherApp.swift
+//  SwiftUIWeather
+//
+//  Created by Balogun Kayode on 17/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
